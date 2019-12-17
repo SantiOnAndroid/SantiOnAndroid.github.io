7c68230515bf9e47fd0667d6d53d9d19612d418b
Santi <santiestudiantitic@gmail.com>
2019-12-17 12:22:06 +0100

    modificant titolj

--- animation.js
+++ animation.js
@@ -1,6 +1,7 @@
 
 $(document).ready(function() {
     $(".title").lettering();
+    animation();
 
 });
 
@@ -21,5 +22,5 @@ function animation() {
     title1.staggerFromTo(".title span", 0.5,
         {ease: Back.easeOut.config(1.7), opacity: 0, bottom: -80},
         {ease: Back.easeOut.config(1.7), opacity: 1, bottom: 0}, 0.05);
- 
+
 }
